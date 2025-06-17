7b91563117547aeda617139425f3dbba30805648
Hadiah <hkvenner@gmail.com>
2025-06-17 12:29:00 -0400

    added cartitems component

--- src/CartSlice.jsx
+++ src/CartSlice.jsx
@@ -11,16 +11,13 @@ export const CartSlice = createSlice({
         // Check if the item already exists in the cart by comparing names
         const existingItem = state.items.find(item => item.name === name);
         if (existingItem) {
-            // If item already exists in the cart, increase its quantity
-            existingItem.quantity++;
+          // If item already exists in the cart, increase its quantity
+          existingItem.quantity++;
         } else {
-            // If item does not exist, add it to the cart with quantity 1
-            state.items.push({name, image, cost, quantity: 1})
+          // If item does not exist, add it to the cart with quantity 1
+          state.items.push({ name, image, cost, quantity: 1 });
         }
-    },
-    removeItem: (state, action) => {
-        state.items = state.items.filter(item => item.name !== action.payload);
-    },
+      },
     updateQuantity: (state, action) => {
         const {name, quantity} = action.payload; // Destructure the product name and new quantity from the action payload
         // Find the item in the cart that matches the given name
